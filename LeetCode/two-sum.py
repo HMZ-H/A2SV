554615f3888f7381df06885d85356daf28fe2583
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2023-12-26 18:09:27 +0300

    Added solution for Two Sum

---
+++ LeetCode/two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+
+        for key, value in enumerate(nums):
+            r = target - value
+            if r in dic: 
+                return [dic[r], key]
+            dic[value] = key
+
+        

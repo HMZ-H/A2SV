21ae0b5b9b1528c5c7535dbb5537143e60f5fbbb
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2023-12-28 10:28:56 +0300

    Added solution for Design Authentication Manager

---
+++ LeetCode/design-authentication-manager.py
@@ -0,0 +1,30 @@
+class AuthenticationManager:
+
+    def __init__(self, timeToLive: int):
+        self.ttl = timeToLive
+        self.tokens= {}
+        
+
+    def generate(self, tokenId: str, currentTime: int) -> None:
+        self.tokens[tokenId] = currentTime + self.ttl
+        
+
+    def renew(self, tokenId: str, currentTime: int) -> None:
+        if tokenId in self.tokens and currentTime < self.tokens[tokenId]:
+            self.tokens[tokenId] = currentTime + self.ttl
+        
+
+    def countUnexpiredTokens(self, currentTime: int) -> int:
+        count = 0
+        for value in self.tokens.values():
+            if value > currentTime:
+                count +=1
+        return count
+        
+
+
+# Your AuthenticationManager object will be instantiated and called as such:
+# obj = AuthenticationManager(timeToLive)
+# obj.generate(tokenId,currentTime)
+# obj.renew(tokenId,currentTime)
+# param_3 = obj.countUnexpiredTokens(currentTime)

688e67ff4ea6670ee378ea1db65837b2c55f0dc7
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2024-01-08 14:21:27 +0300

    Added solution for Pancake Sorting

---
+++ LeetCode/pancake-sorting.py
@@ -0,0 +1,17 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        
+        flips = []
+
+        for i in range(len(arr), 0, -1):
+            index = arr.index(i)
+
+            if index != i - 1:
+                if index != 0:
+                    arr[:index + 1] = reversed(arr[:index + 1])
+                    flips.append(index + 1)
+                    
+                arr[:i] = reversed(arr[:i])
+                flips.append(i)
+
+        return flips

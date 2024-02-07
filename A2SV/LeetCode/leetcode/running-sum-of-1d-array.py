edf733c18781e289a6daab50ebcc115e594bc6aa
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2024-02-07 14:52:59 +0300

    Add solution for Running Sum of 1d Array

--- A2SV/LeetCode/leetcode/running-sum-of-1d-array.py
+++ A2SV/LeetCode/leetcode/running-sum-of-1d-array.py
@@ -4,3 +4,9 @@ class Solution:
         for i in nums:
             res.append(i+res[-1])
         return res[1:]
+        # l = nums[0]
+        # res = []
+        # res.append(l)
+        # for i in range(1, len(nums)):
+        #     res.append(res[-1] + nums[i])
+        # return res

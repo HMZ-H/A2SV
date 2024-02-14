6341c0563089443e20d6234a5ca5d8c701f67ce5
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2024-02-14 19:23:25 +0300

    Add solution for Palindrome Linked List

--- A2SV/LeetCode/leetcode/palindrome-linked-list.py
+++ A2SV/LeetCode/leetcode/palindrome-linked-list.py
@@ -5,11 +5,29 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        lis = []
-        curr = head
-        while curr:
-            lis.append(curr.val)
-            curr = curr.next
+        # lis = []
+        # curr = head
+        # while curr:
+        #     lis.append(curr.val)
+        #     curr = curr.next
         
-        return lis == lis[::-1]
+        # return lis == lis[::-1]
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        prev = None
+        while slow:
+            curr = slow.next
+            slow.next = prev
+            prev = slow
+            slow = curr
+        l, r = head, prev
+        while r:
+            if l.val != r.val:
+                return False
+            l = l.next
+            r = r.next
+        return True
         

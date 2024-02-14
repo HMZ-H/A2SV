8dcb42fa3fac2e6cb3364dccc0cda5bdc8735982
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2024-02-14 19:53:22 +0300

    Add solution for Remove Duplicates from Sorted List

--- A2SV/LeetCode/leetcode/remove-duplicates-from-sorted-list.py
+++ A2SV/LeetCode/leetcode/remove-duplicates-from-sorted-list.py
@@ -5,15 +5,16 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head is None or head.next is None:
-            return head
-        curr = head
-        
-        while curr and curr.next:
-            if curr.val == curr.next.val:
+        output=curr = head
+        # while curr:
+        #     while curr.next and curr.val==curr.next.val:
+        #         curr.next=curr.next.next
+        #     curr=curr.next
+        # return output
+        while curr:
+            if curr.next and curr.val == curr.next.val:
                 curr.next = curr.next.next
             else:
                 curr = curr.next
-        return head
-
+        return output
         

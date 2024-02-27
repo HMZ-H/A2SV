5f6f8c3acf8502404962392b278a55dda67b65d5
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2024-02-27 17:10:04 +0300

    Add solution for Search in a Binary Search Tree

---
+++ A2SV/LeetCode/leetcode/search-in-a-binary-search-tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        
+        temp = None
+        if not root:
+            return None
+        if root:
+            if root.val == val:
+                return root
+            elif root.val < val:
+                return self.searchBST(root.right, val)
+            else:
+                return self.searchBST(root.left, val)
+        return None
+
+        
+        # def dfs(root):
+        #     nonlocal temp
+        #     if not root:
+        #         return root
+        #     if root:
+        #         if root.val == val:
+        #             temp = root
+        #         dfs(root.left)
+        #         dfs(root.right)
+        # dfs(root)
+        # return temp

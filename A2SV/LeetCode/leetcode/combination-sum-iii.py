b93820f92fc048049dadbafa5314901662b35ac1
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2024-03-01 18:25:11 +0300

    Add solution for Combination Sum III

--- A2SV/LeetCode/leetcode/combination-sum-iii.py
+++ A2SV/LeetCode/leetcode/combination-sum-iii.py
@@ -1,14 +1,19 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         res = []
-        def backt(index, path=[]):
-            if len(path) > k:
-                return 
-            if len(path) == k and sum(path) == n:
+        def backt(index, path=[], sums=0):
+            print(path)
+            if len(path) == k and sums == n:
                 res.append(path[:])
-            for i in range(index, 10):
-                path.append(i)
-                backt(i + 1, path)
-                path.pop()
+                return
+            if len(path) >= k or sums > n:
+                return 
+            for i in range(index, min(n+1, 10)):
+                if sums <= n:
+                    path.append(i)
+                    sums +=i
+                    backt(i + 1, path, sums)
+                    path.pop()
+                    sums-=i
         backt(1)
         return res        

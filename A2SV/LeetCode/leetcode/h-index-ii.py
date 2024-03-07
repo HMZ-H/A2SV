9f8fe34826f27caa595417e6a2788d2398ce24be
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2024-03-07 08:41:38 +0300

    Add solution for H-Index II

--- A2SV/LeetCode/leetcode/h-index-ii.py
+++ A2SV/LeetCode/leetcode/h-index-ii.py
@@ -1,11 +1,25 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.reverse()
+        # citations.reverse()
+        # if len(citations) <= citations[-1]:
+        #     return len(citations)
+        # for i in range(len(citations)):
+        #     if citations[i] < i + 1:
+        #         return i
+        # return 0
         if len(citations) == 1 and citations[0] > 0:
             return 1
-        if len(citations) <= citations[-1]:
+        if citations[-1] == 0:
+            return 0
+        if len(citations)<= citations[0]:
             return len(citations)
-        for i in range(len(citations)):
-            if citations[i] < i + 1:
-                return i
-        return 0
+        left, right = 0, len(citations)
+        best = 0
+        while left <= right:
+            mid = left + (right - left)//2
+            if mid>= citations[len(citations) - mid -1]:
+                best = mid
+                right = mid -1
+            else:
+                left = mid +1
+        return best

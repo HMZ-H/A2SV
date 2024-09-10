b24f8355ede71f45d201e392548269ab75c84220
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2024-09-10 10:53:43 +0300

    Created 'Binary Search' by A2SV Hub

---
+++ 10-Sep-2024/Binary Search 214000.py
@@ -0,0 +1,21 @@
+# Problem: Binary Search - https://leetcode.com/problems/binary-search/description/
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+
+        low, high = 0, len(nums) -1
+
+        while low <= high:
+            mid = (low + high)//2
+            # print(low, high)
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid -1
+        
+
+        return -1
+        

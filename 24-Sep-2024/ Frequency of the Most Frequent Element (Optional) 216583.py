0fda305253b12e8880912aa46f62d8c726a1abf4
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2024-09-24 22:59:33 +0300

    Created ' Frequency of the Most Frequent Element (Optional)' by A2SV Hub

---
+++ 24-Sep-2024/ Frequency of the Most Frequent Element (Optional) 216583.py
@@ -0,0 +1,20 @@
+# Problem:  Frequency of the Most Frequent Element (Optional) - https://leetcode.com/problems/frequency-of-the-most-frequent-element/
+
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        left = 0
+        ans = 0
+        curr = 0
+        
+        for right in range(len(nums)):
+            target = nums[right]
+            curr += target
+            
+            while (right - left + 1) * target - curr > k:
+                curr -= nums[left]
+                left += 1
+            
+            ans = max(ans, right - left + 1)
+
+        return ans

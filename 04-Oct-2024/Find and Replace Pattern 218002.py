fc4db794ef1d0846bbf1a33064092ac6d0bb1fc3
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2024-10-04 16:30:49 +0300

    Created 'Find and Replace Pattern' by A2SV Hub

---
+++ 04-Oct-2024/Find and Replace Pattern 218002.py
@@ -0,0 +1,28 @@
+# Problem: Find and Replace Pattern - https://leetcode.com/problems/find-and-replace-pattern/
+
+class Solution:
+    def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
+        ans = []
+        for word in words:
+            dic = {}
+            dic2 = {}
+            f = True
+            for i in range(len(word)):
+                if word[i] in dic:
+                    if dic[word[i]]!=pattern[i]:
+                        f = False
+                        break
+                    else:
+                        continue
+                elif pattern[i] in dic2:
+                    if dic2[pattern[i]]!=word[i]:
+                        f = False
+                        break
+                    else:
+                        continue
+                else:
+                    dic[word[i]] = pattern[i]
+                    dic2[pattern[i]] = word[i]
+            if f:
+                ans.append(word)
+        return ans

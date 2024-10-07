ff85a6b720ddc038facb005bc0da678dbefba885
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2024-10-07 23:27:22 +0300

    Created 'Subdomain Visit Count' by A2SV Hub

---
+++ 07-Oct-2024/Subdomain Visit Count 218738.py
@@ -0,0 +1,22 @@
+# Problem: Subdomain Visit Count - https://leetcode.com/problems/subdomain-visit-count/description/
+
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+       
+        res = {}
+        for i in range(len(cpdomains)):
+            visit_num = cpdomains[i].split(' ')[0]
+            domains = cpdomains[i].split(' ')[1]
+            domains = domains.split('.')[::-1]
+            k = 1
+            while k < len(domains)+1:
+                dom = '.'.join(domains[:k][::-1])
+                if dom in res:
+                    res[dom] += int(visit_num)
+                else:
+                    res[dom] = int(visit_num)
+                k +=1
+        ans = []
+        for key, value in res.items():
+            ans.append(str(value) + ' ' + key)
+        return ans

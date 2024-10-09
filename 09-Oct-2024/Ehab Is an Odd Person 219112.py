2c36e95b43f6eadba6a1613640cd27d11fbb76da
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2024-10-09 23:10:42 +0300

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 09-Oct-2024/Ehab Is an Odd Person 219112.py
@@ -0,0 +1,17 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+def main():
+    n = int(input()) 
+    arr = list(map(int, input().split()))
+    
+    ex = [False, False]  
+    for num in arr:
+        ex[num % 2] = True
+    
+    if ex[0] and ex[1]:
+        arr.sort()
+    
+    print(" ".join(map(str, arr)))
+
+if __name__ == "__main__":
+    main()

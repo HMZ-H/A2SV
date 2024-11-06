a12a4e5e314617b7afc5733fd6b409b6d7598c55
Hamza Haji <119335393+HMZ-H@users.noreply.github.com>
2024-11-06 11:02:12 -0800

    Created 'Separate Black and white balls' by A2SV Hub

---
+++ 06-Nov-2024/Separate Black and white balls 223399.py
@@ -0,0 +1,16 @@
+# Problem: Separate Black and white balls - https://leetcode.com/problems/separate-black-and-white-balls/
+
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+
+        
+        cout, res = 0,0
+        for i in range(len(s)-1,-1,-1):
+
+            if s[i] == '0':
+                cout +=1
+            else:
+                res +=cout
+        return res
+
+        
